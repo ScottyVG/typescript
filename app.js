071951917a190f0a027ef868eb1616169a2c74e5
scottyvg <svangilder@gmail.com>
2017-02-09 10:25:29 -0700

    basic typescript

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("It works!");
+// string
+var myName = 'Scotty';
+// myName = 28;
+// number
+var myAge = 31.5;
+// myAge = 'Scotty';
+// boolean
+var hasHobbies = true;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 31;
+// myRealAge = '31';
+// array
+var hobbies = ["Cooking", "Sports"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log("It works!");
+// string
+let myName: string = 'Scotty';
+// myName = 28;
+
+// number
+let myAge: number = 31.5;
+// myAge = 'Scotty';
+
+// boolean
+let hasHobbies: boolean = true;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 31;
+// myRealAge = '31';
+
+// array
+let hobbies: any[] = ["Cooking", "Sports"];
+// console.log(hobbies[0]);
+// console.log(typeof hobbies);
+hobbies = ["string"];
+
+// tuples
