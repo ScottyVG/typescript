a52af9abc3dc6a2fe8a1e8dcfd783e63012b2a76
scottyvg <svangilder@gmail.com>
2017-02-10 10:58:48 -0700

    complete section 3 - understanding the typescript compiler

--- section-3/app.js
+++ section-3/app.js
@@ -2,5 +2,13 @@ var myName = 'Scotty';
 var myAge = 31;
 var anything;
 anything = 12;
-// myName = 30; 
+// myName = 30;
+function controlMe(isTrue, somethingElse) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    result == 33;
+    return result;
+}
 //# sourceMappingURL=app.js.map
--- section-3/app.ts
+++ section-3/app.ts
@@ -5,10 +5,11 @@ anything = 12;
 
 // myName = 30;
 
-function controlMe(isTrue: boolean) {
+function controlMe(isTrue: boolean, somethingElse: boolean) {
   let result: number;
   if (isTrue) {
     result = 12;
   }
+  result == 33;
   return result;
 }
