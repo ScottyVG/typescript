3e4fa54379c9c47dda881d97f5451342070adb78
scottyvg <svangilder@gmail.com>
2017-02-10 10:34:45 -0700

    setup for multiple folders, add completed sections to tsconfig exclusions

---
+++ section-3/app.js
@@ -0,0 +1,6 @@
+var myName = 'Scotty';
+var myAge = 31;
+var anything;
+anything = 12;
+// myName = 30; 
+//# sourceMappingURL=app.js.map
---
+++ section-3/app.ts
@@ -0,0 +1,14 @@
+let myName: string = 'Scotty';
+let myAge: number = 31;
+let anything: any;
+anything = 12;
+
+// myName = 30;
+
+function controlMe(isTrue: boolean) {
+  let result: number;
+  if (isTrue) {
+    result = 12;
+  }
+  return result;
+}
