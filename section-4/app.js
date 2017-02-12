ea0ddfb3fdbf2a274b35b6f3a44cd4147df782bb
scottyvg <svangilder@gmail.com>
2017-02-12 12:22:10 -0700

    complete section 4 - typescript and es6

--- section-4/app.js
+++ section-4/app.js
@@ -6,7 +6,7 @@ variable = "Another value";
 console.log(variable);
 var maxLevels = 100;
 console.log(maxLevels);
-maxLevels = 99; // Won't work
+// maxLevels = 99; // Won't work
 // Block scope
 function reset() {
     // console.log(variable);
@@ -23,3 +23,49 @@ var addNumbers = function (number1, number2) {
 console.log(addNumbers(10, 3));
 var multiplyNumbers = function (number1, number2) { return number1 * number2; };
 console.log(multiplyNumbers(10, 3));
+var greet = function () {
+    console.log("Hello");
+};
+greet();
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend("Manu");
+// Default Parameters
+console.log("DEFAULT PARAMETERS");
+var countdown = function (start) {
+    if (start === void 0) { start = 10; }
+    while (start > 0) {
+        start--;
+    }
+    console.log("Done", start);
+};
+// countdown(10);
+countdown();
+// Rest & Spread
+console.log("REST & SPREAD");
+var numbers = [1, 10, 99, -5];
+console.log(Math.max.apply(Math, numbers));
+function makeArray() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args;
+}
+// console.log(makeArray("Scotty", 1, 2, 3, 6));
+// Destructuring
+console.log("DESTRUCTURING");
+var myHobbies = ["Cooking", "Sports"];
+// const hobby1 = myHobbies[0]
+// const hobby2 = myHobbies[1]
+// console.log(hobby1, hobby2);
+var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+console.log(hobby1, hobby2);
+var userData = { userName: "Scotty", age: 31 };
+// const userName = userData.userName;
+// const age = userData.age;
+var myName = userData.userName, myAge = userData.age;
+console.log(myName, myAge);
+// Template Literals
+var userName = "Scotty";
+var greeting = "Hello, I'm " + userName;
+console.log(greeting);
--- section-4/app.ts
+++ section-4/app.ts
@@ -8,7 +8,7 @@ console.log(variable);
 
 const maxLevels = 100;
 console.log(maxLevels);
-maxLevels = 99; // Won't work
+// maxLevels = 99; // Won't work
 
 // Block scope
 function reset() {
@@ -29,3 +29,52 @@ console.log(addNumbers(10, 3));
 
 const multiplyNumbers = (number1: number, number2: number) => number1 * number2;
 console.log(multiplyNumbers(10, 3));
+
+const greet = () => {
+  console.log("Hello");
+};
+greet()
+
+const greetFriend = friend => console.log(friend);
+greetFriend("Manu");
+
+// Default Parameters
+console.log("DEFAULT PARAMETERS");
+const countdown = (start: number = 10): void => {
+  while (start > 0) {
+    start--;
+  }
+  console.log("Done", start);
+};
+// countdown(10);
+countdown();
+
+// Rest & Spread
+console.log("REST & SPREAD");
+const numbers = [1, 10, 99, -5];
+console.log(Math.max(...numbers));
+
+function makeArray(...args: number[]) {
+  return args;
+}
+// console.log(makeArray("Scotty", 1, 2, 3, 6));
+
+// Destructuring
+console.log("DESTRUCTURING");
+const myHobbies = ["Cooking", "Sports"];
+// const hobby1 = myHobbies[0]
+// const hobby2 = myHobbies[1]
+// console.log(hobby1, hobby2);
+const [ hobby1, hobby2 ] = myHobbies;
+console.log(hobby1, hobby2);
+
+const userData = { userName: "Scotty", age: 31 };
+// const userName = userData.userName;
+// const age = userData.age;
+const {userName: myName, age:myAge} = userData;
+console.log(myName, myAge);
+
+// Template Literals
+const userName = "Scotty";
+const greeting = `Hello, I'm ${userName}`;
+console.log(greeting);
