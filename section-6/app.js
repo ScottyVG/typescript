7f1898e7c3298181941ea4d4e5a8e93f068f1e75
scottyvg <svangilder@gmail.com>
2017-02-14 08:15:40 -0700

    system JS setup and working

---
+++ section-6/app.js
@@ -0,0 +1,6 @@
+define(["require", "exports", "./math/circle"], function (require, exports, circle_1) {
+    "use strict";
+    console.log(circle_1.PI);
+    console.log(circle_1.calculateCircumference(10));
+});
+// console.log();
---
+++ section-6/app.ts
@@ -0,0 +1,6 @@
+import { PI, calculateCircumference } from "./math/circle";
+
+console.log(PI);
+console.log(calculateCircumference(10));
+// console.log();
+
