792e1adbbd0272d5bd13d14c67480aec95bb56b3
scottyvg <svangilder@gmail.com>
2017-02-15 09:56:09 -0700

    interfaces with class notation

--- section-7/app.js
+++ section-7/app.js
@@ -6,8 +6,26 @@ function changeName(person) {
 }
 var person = {
     firstName: "Scotty",
-    age: 31
+    // age: 31
+    hobbies: ["hiking", "biking"],
+    greet: function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    }
 };
-greet(person);
+// greet({firstName: "Max", age: 27});
 changeName(person);
 greet(person);
+person.greet("Anything");
+var Person = (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    };
+    ;
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = "Scotty";
+greet(myPerson);
+myPerson.greet("Anything");
--- section-7/app.ts
+++ section-7/app.ts
@@ -1,5 +1,8 @@
 interface NamedPerson {
   firstName: string;
+  age?: number;
+  [propName: string]: any;
+  greet(lastName: string): void;
 }
 
 function greet(person: NamedPerson) {
@@ -10,11 +13,29 @@ function changeName(person: NamedPerson) {
   person.firstName = "Brightin";
 }
 
-const person = {
+const person: NamedPerson = {
   firstName: "Scotty",
-  age: 31
+  // age: 31
+  hobbies: ["hiking", "biking"],
+  greet(lastName: string) {
+    console.log("Hi, I am " + this.firstName + " " + lastName);
+  }
 };
 
-greet(person);
+// greet({firstName: "Max", age: 27});
 changeName(person);
 greet(person);
+person.greet("Anything");
+
+class Person implements NamedPerson {
+  firstName: string;
+  lastName: string;
+  greet(lastName: string) {
+    console.log("Hi, I am " + this.firstName + " " + lastName);
+  };
+}
+
+const myPerson = new Person();
+myPerson.firstName = "Scotty";
+greet(myPerson);
+myPerson.greet("Anything");
